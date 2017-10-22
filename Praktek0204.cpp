671a74ec556d53cae77d1d8285de47420dfb5a7e
RizaldiAkhmad <32384084+RizaldiAkhmad@users.noreply.github.com>
2017-10-22 00:19:57 -0700

    menghitung luas rumah dan luas 1 box parket

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -6,5 +6,7 @@ int main(){
 	double luasRumah = 42 ;
 	double lantaiParket =193*1215 ;
 	double luas1box = lantaiParket*8/1000000;
+	cout << 'Luas Rumah  :'<<luasRumah<<"m2"<<endl;
+	cout << 'Luas 1 box parket :'<<luas1box<<"m2"<<endl;
 	
 }
