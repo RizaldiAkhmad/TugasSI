5a7771a0e77ae9cc593aae2cc1c3a18c25b12320
RizaldiAkhmad <32384084+RizaldiAkhmad@users.noreply.github.com>
2017-10-22 00:25:54 -0700

    Menghitung dan Menampilkan Jumlah Box Parket

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -6,9 +6,11 @@ int main(){
 	double luasRumah = 42 ;
 	double lantaiParket =193*1215 ;
 	double luas1box = lantaiParket*8/1000000;
-	cout << 'Luas Rumah  :'<<luasRumah<<"m2"<<endl;
+	cout << 'Luas Rumah        :'<<luasRumah<<"m2"<<endl;
 	cout << 'Luas 1 box parket :'<<luas1box<<"m2"<<endl;
 	
 	double jumlahBoxP,biayaParket,biayaPasang,totalBiaya;
+	jumlahBoxP = luasRumah/luas1box;
+	cout << "Jumlah Box Parket   :"<<jumlahBoxP<<endl;
 	
 }
